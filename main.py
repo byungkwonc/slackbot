d98cdf38c0cf3fc296861223ef02c70cf1a28617
최병권 <byungkwonc@gmail.com>
2023-11-22 15:28:19 +0900

    add main.py

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ import os
 #pip install slack_bolt
 from slack_bolt import App
 
+token = os.environ.get("SLACK_BOT_TOKEN")
+signing_secret = os.environ.get("SLACK_SIGNING_SECRET") 
+
+print("SLACK_BOT_TOKEN=", token)
+
 app = App(
     token = os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret = os.environ.get("SLACK_SIGNING_SECRET") 
 )
 
+# Listens to incoming messages that contain "hello"
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+# Start your app
 if __name__ == "__main__":
-    app.start(
-        port = int(os.environ.get("PORT", 3000))
-    )
+    app.start(port=int(os.environ.get("PORT", 3000)))
